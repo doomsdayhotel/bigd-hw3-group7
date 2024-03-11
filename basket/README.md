5862a216820ba26b1a590c3f47107639ce06224f
Chloe Kwon <chloekwon96@gmail.com>
2024-03-11 14:48:55 -0400

    Implement MRBasket and update part 1 of README

--- basket/README.md
+++ basket/README.md
@@ -2,7 +2,14 @@
 
 ## Explain each step of your solution
 
-...
+1. Parse data to generate tuples of each `basket` and `items`, where a single `basket` is identified as a unique `user` 
+and `date` combination, since all activity by a user on the same day
+corresponds to a single `basket`. 
+2. Aggregate to get a set of all unique `items` for each (`user_id`, `date`) tuple (`basket`) to remove duplicates.
+3. For each `basket`, generate all combination of `item` pairs and count co-occurrence.
+4. Across all (`user_id`, `date`) combination `baskets` sum the co-occurrence count for each `item` pair.
+5. Reformat so that key is `item_id` and value is (`item_id`, `co-occurrence count`) and the item with the highest 
+co-occurrence count.
 
 ## What problems, if any, did you encounter?
 
--- basket/src/mr_basket.py
+++ basket/src/mr_basket.py
@@ -2,6 +2,8 @@
 
 from mrjob.job import MRJob
 from mrjob.step import MRStep
+from itertools import combinations
+from operator import itemgetter
 
 
 class MRBasket(MRJob):
@@ -23,17 +25,40 @@ class MRBasket(MRJob):
                 (key, value) pairs
         """
 
-        # TODO: start implementing here!
-        # ...
-        # yield ...
-        pass
+        user_id, date, item_name = line.split(', ')
+        yield (user_id, date), item_name
+
+    def reducer_group_by_basket(self, basket, items):
+        # Remove duplicate items in basket
+        yield basket, list(set(items))
+
+    def mapper_generate_item_pairs(self, basket, items):
+        # Generate co-occurring item pairs
+        for item_a, item_b in combinations(items, 2):
+            yield (item_a, item_b), 1
+            yield (item_b, item_a), 1
+
+    def reducer_count_co_occurrence(self, item_pair, counts):
+        # Sum counts for each co-occurring item pairs
+        yield item_pair[0], (item_pair[1], sum(counts))
+
+    def mapper_reformat_key_value(self, item, co_occurrence):
+        # co_occurrence is a tuple of (paired item, co-occurrence count)
+        yield item, co_occurrence
+
+    def reducer_find_max_co_occurrence(self, item, co_occurrence):
+        # Get the tuple of (paired item, co-occurrence count) where co-occurrence count is highest for item
+        most_common = max(co_occurrence, key=itemgetter(1))
+        yield item, most_common
 
     def steps(self):
         return [
-            MRStep(
-                mapper=self.mapper_get_session_items,
-                #reducer=self.???
-            ),
+            MRStep(mapper=self.mapper_get_session_items,
+                   reducer=self.reducer_group_by_basket),
+            MRStep(mapper=self.mapper_generate_item_pairs,
+                   reducer=self.reducer_count_co_occurrence),
+            MRStep(mapper=self.mapper_reformat_key_value,
+                   reducer=self.reducer_find_max_co_occurrence)
         ]
 
 
